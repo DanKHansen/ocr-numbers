7045217e39ed5fd19f4992016d3228bf1d5204cd
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-13 10:26:28 +0200

    solution

--- src/main/scala/OcrNumbers.scala
+++ src/main/scala/OcrNumbers.scala
@@ -1,5 +1,8 @@
+import java.util.InvalidPropertiesFormatException
+
 object OcrNumbers:
-   def convert(l: List[String]): String = {
+
+   def convert(l: List[String]): String =
       val ocrs = Map(
         List(" _ ", "| |", "|_|", "   ") -> "0",
         List("   ", "  |", "  |", "   ") -> "1",
@@ -12,5 +15,15 @@ object OcrNumbers:
         List(" _ ", "|_|", "|_|", "   ") -> "8",
         List(" _ ", "|_|", " _|", "   ") -> "9"
       )
-      ocrs.getOrElse(l,"?")
-   }
+
+      val lines = l.sliding(4, 4)
+      val numbers = lines.map(l =>
+         if l.size % 4 != 0 || l.forall(_.length % 3 != 0) then "?"
+         else
+            val tops = l.head.sliding(3, 3).toList
+            val upperMiddles = l(1).sliding(3, 3).toList
+            val lowerMiddles = l(2).sliding(3, 3).toList
+            val bottoms = l(3).sliding(3, 3).toList
+            val cubes = for i <- tops.indices yield tops(i) :: upperMiddles(i) :: lowerMiddles(i) :: bottoms(i) :: Nil
+            cubes.map(c => ocrs.getOrElse(c, "?")).mkString)
+      numbers.mkString(",")
